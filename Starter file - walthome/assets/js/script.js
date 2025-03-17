6c83189cc5a8e1cd717796bdb934ba67230e6835
Gurleen Kaur <130543552+gurleenk24@users.noreply.github.com>
2025-03-17 19:14:19 +0000

    commit

---
+++ Starter file - walthome/assets/js/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+//  navbar toggle in mobile(responsive)
+
+const /* {NodeElement} */ $navbar = document.querySelector("[data-navbar]");
+const /* {NodeElement}  */ $navToggler = document.querySelector("[data-nav-toggler]");
